2a3c25a8cf96e93d0653f393f19f039617f9319d
Linus Brian Okoth <okoth@fb.com>
2020-07-06 17:34:00 -0400

    Created accurate binding

--- app/src/main/java/com/example/linusgram/LoginActivity.java
+++ app/src/main/java/com/example/linusgram/LoginActivity.java
@@ -9,6 +9,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_login);
 
 
--- app/src/main/java/com/example/linusgram/MainActivity.java
+++ app/src/main/java/com/example/linusgram/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.linusgram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.Bindable;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ActivityMainBinding binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+
     }
 }
