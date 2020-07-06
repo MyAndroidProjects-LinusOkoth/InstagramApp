f54c9d8bc11d552e49907203bf99dfca9c0d88e0
Linus Brian Okoth <okoth@fb.com>
2020-07-06 18:08:31 -0400

    Added Login Check

--- app/src/main/java/com/example/linusgram/LoginActivity.java
+++ app/src/main/java/com/example/linusgram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.linusgram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -40,15 +41,24 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser(String username, String password) {
-        Toast.makeText(LoginActivity.this, "login in"+username, Toast.LENGTH_SHORT).show();
 
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with login" + e);
+                }
+                Toast.makeText(LoginActivity.this, "login in", Toast.LENGTH_SHORT).show();
 
+                goToMainActivity();
 
             }
         });
 
+    }
+
+    private void goToMainActivity() {
+
+        Intent intent =
     }
 }
