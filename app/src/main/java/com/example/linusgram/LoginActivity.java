e23043b9908a109e896b4c2e57eeeffbb1b10cd4
Linus Brian Okoth <okoth@fb.com>
2020-07-06 17:26:06 -0400

    Enabled binding

--- app/src/main/java/com/example/linusgram/LoginActivity.java
+++ app/src/main/java/com/example/linusgram/LoginActivity.java
@@ -10,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
     }
 }
