5a5b7dd577b3b5f3085bbac2c6af0d9106cbbea0
Linus Brian Okoth <okoth@fb.com>
2020-07-06 17:52:21 -0400

    Finished login activity

--- app/src/main/java/com/example/linusgram/LoginActivity.java
+++ app/src/main/java/com/example/linusgram/LoginActivity.java
@@ -3,22 +3,41 @@ package com.example.linusgram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.linusgram.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        final ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
 
         // layout of activity is stored in a special property called root
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "OnClick login button");
+                String username = binding.etUsername.getText().toString();
+                String password = binding.etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+    }
+
+    private void loginUser(String username, String password) {
+        Toast.makeText(LoginActivity.this, "login in"+username, Toast.LENGTH_SHORT).show();
 
     }
 }
