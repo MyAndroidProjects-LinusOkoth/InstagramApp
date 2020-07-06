0b74484722a4859775d6019830073145f4989eb0
Linus Brian Okoth <okoth@fb.com>
2020-07-06 18:00:45 -0400

    Added parse innitialization

--- app/src/main/java/com/example/linusgram/ParseApplication.java
+++ app/src/main/java/com/example/linusgram/ParseApplication.java
@@ -1,4 +1,23 @@
 package com.example.linusgram;
 
-public class ParseApplication {
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("myAppId") // should correspond to APP_ID env variable
+                .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
+                .clientBuilder(builder)
+                .server("https://my-parse-app-url.herokuapp.com/parse/").build());
+
+    }
 }
