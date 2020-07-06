3d8922507b3d934b0dacc953d6362fd268444b67
Linus Brian Okoth <okoth@fb.com>
2020-07-06 17:35:42 -0400

    Added binding to main

--- app/src/main/java/com/example/linusgram/MainActivity.java
+++ app/src/main/java/com/example/linusgram/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.databinding.Bindable;
 
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.view.View;
+
+import com.example.linusgram.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ActivityMainBinding binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+
+        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        // layout of activity is stored in a special property called root
+        View view = binding.getRoot();
+        setContentView(view);
+
 
     }
 }
