899d35ff40b643cf021c20c30fa459f12c17d71d
Linus Brian Okoth <okoth@fb.com>
2020-07-06 21:55:11 -0400

    Added parseclass name

--- app/src/main/java/com/example/linusgram/Post.java
+++ app/src/main/java/com/example/linusgram/Post.java
@@ -1,9 +1,11 @@
 package com.example.linusgram;
 
+import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+@ParseClassName("Post")
 public class Post extends ParseObject {
 
     public static final String KEY_DESCRIPTION = "description";
