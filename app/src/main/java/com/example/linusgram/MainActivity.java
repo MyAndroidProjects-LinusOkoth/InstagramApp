4ea3706879e0a2bdd34a6a450e4bbfc742e647c1
Linus Brian Okoth <okoth@fb.com>
2020-07-06 23:13:59 -0400

    Saved post

--- app/src/main/java/com/example/linusgram/MainActivity.java
+++ app/src/main/java/com/example/linusgram/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.linusgram.databinding.ActivityMainBinding;
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -29,8 +33,42 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        queryPost();
+        //queryPost();
 
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String description = binding.etDescription.getText().toString();
+                if(description.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Description cannot be empty",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description, currentUser);
+                binding.etDescription.setText("");
+
+            }
+        });
+
+
+    }
+
+    private void savePost(String description, ParseUser currentUser) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setUser(currentUser);
+
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error saving post ", e);
+                    return;
+                }
+                Log.i(TAG, "Post was saved");
+            }
+        });
 
     }
 
