e31fac27c93842332738dcea16181957243a9e90
Linus Brian Okoth <okoth@fb.com>
2020-07-06 21:48:00 -0400

    Resgistered the post class

--- app/src/main/java/com/example/linusgram/ParseApplication.java
+++ app/src/main/java/com/example/linusgram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.linusgram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ///Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
