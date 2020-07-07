682ef8952a5ab7f4cb3953f1d08c8c6d2e22b7bf
Linus Brian Okoth <okoth@fb.com>
2020-07-06 22:52:52 -0400

    Added username

--- app/src/main/java/com/example/linusgram/MainActivity.java
+++ app/src/main/java/com/example/linusgram/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.linusgram;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.View;
 
 import com.example.linusgram.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        queryPost();
+
 
     }
+
+    private void queryPost(){
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error getting Post", e);
+                    return;
+                }
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + " username: " + post.getUser().getUsername());
+                }
+
+            }
+        });
+    }
+
+
 }
