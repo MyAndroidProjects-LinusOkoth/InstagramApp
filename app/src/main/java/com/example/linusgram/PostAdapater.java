5a40a5aa1748024ca68d9097e6c86dbfa2f8eb37
Linus Brian Okoth <okoth@fb.com>
2020-07-07 10:17:41 -0400

    Configured Adapter

--- app/src/main/java/com/example/linusgram/PostAdapater.java
+++ app/src/main/java/com/example/linusgram/PostAdapater.java
@@ -1,6 +1,41 @@
 package com.example.linusgram;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PostAdapater extends RecyclerView.Adapter<> {
+import java.util.List;
+
+public class PostAdapater extends RecyclerView.Adapter<PostAdapater.ViewHolder> {
+
+    private Context context;
+    private List list;
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends  RecyclerView.ViewHolder{
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
