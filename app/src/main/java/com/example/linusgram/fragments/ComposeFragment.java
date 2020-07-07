9f2a76d4612d5e72b921ced4d99c616f6ec33935
Linus Brian Okoth <okoth@fb.com>
2020-07-07 09:07:42 -0400

    Added get context method

--- app/src/main/java/com/example/linusgram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/linusgram/fragments/ComposeFragment.java
@@ -126,7 +126,7 @@ public class ComposeFragment extends Fragment {
                 // Load the taken image into a preview
                 ivPostImage.setImageBitmap(takenImage);
             } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
         }
     }
