8075a4afdf2fc62c072d38a8a7bd054b1e20f7d1
Linus Brian Okoth <okoth@fb.com>
2020-07-07 17:45:49 -0400

    Added features for the overrided querry method

--- app/src/main/java/com/example/linusgram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/linusgram/fragments/ProfileFragment.java
@@ -6,11 +6,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.linusgram.Post;
 import com.example.linusgram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 
 public class ProfileFragment extends HomeFragment {
@@ -18,5 +25,28 @@ public class ProfileFragment extends HomeFragment {
     @Override
     protected void queryPost() {
         super.queryPost();
+
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addAscendingOrder(Post.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error getting Post", e);
+                    return;
+                }
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + " username: " + post.getUser().getUsername());
+                }
+
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+
+
+            }
+        });
+
     }
 }
