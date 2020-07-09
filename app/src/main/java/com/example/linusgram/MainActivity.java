b48cac932bcdc7003f9cba0f03328d00ea1be681
Linus Brian Okoth <okoth@fb.com>
2020-07-09 02:48:15 -0400

    Added toolbar to main activity

--- app/src/main/java/com/example/linusgram/MainActivity.java
+++ app/src/main/java/com/example/linusgram/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.linusgram;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -65,7 +66,13 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        //queryPost();
+        /;
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
 
 
 
