e64e24f5a1ffe70f2601c6389c43c4e4c8700e47
Linus Brian Okoth <okoth@fb.com>
2020-07-09 17:38:46 -0400

    Added binding to reduce boilerplate

--- app/src/main/java/com/example/linusgram/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/linusgram/Activities/DetailsActivity.java
@@ -3,20 +3,31 @@ package com.example.linusgram.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.linusgram.Models.Post;
 import com.example.linusgram.R;
+import com.example.linusgram.databinding.ActivityDetailsBinding;
+import com.example.linusgram.databinding.ActivityMainBinding;
 
 public class DetailsActivity extends AppCompatActivity {
 
     Post post;
+    ActivityDetailsBinding binding
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+
+
+       binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+
+        final View view = binding.getRoot();
+        setContentView(view);
 
         post = getIntent().getParcelableExtra("post");
+
+
 
 
 
