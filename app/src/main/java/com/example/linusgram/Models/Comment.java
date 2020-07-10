2ffbf9ad22075851bbaa11f38aa4d8413845e43b
Linus Brian Okoth <okoth@fb.com>
2020-07-10 19:39:39 -0400

    Added comment headings

--- app/src/main/java/com/example/linusgram/Models/Comment.java
+++ app/src/main/java/com/example/linusgram/Models/Comment.java
@@ -3,6 +3,12 @@ package com.example.linusgram.Models;
 
 import com.parse.ParseClassName;
 
-@ParseClassName("Post")
+@ParseClassName("Comment")
 public class Comment{
+
+    public static final String KEY_POST = "RelatedPost";
+    public static final String KEY_USER = "CommentingUser";
+
+
+
 }
