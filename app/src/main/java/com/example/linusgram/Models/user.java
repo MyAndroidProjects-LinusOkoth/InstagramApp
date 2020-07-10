a1568fcecd37644b1dd6546957523372fc2e8122
Linus Brian Okoth <okoth@fb.com>
2020-07-09 23:49:56 -0400

    Added profile pic getter method

--- app/src/main/java/com/example/linusgram/Models/user.java
+++ app/src/main/java/com/example/linusgram/Models/user.java
@@ -2,7 +2,18 @@ package com.example.linusgram.Models;
 
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 
 @ParseClassName("Post")
 public class user {
+
+    public static final String KEY_DESCRIPTION = "ProfilePic";
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_DESCRIPTION);
+    }
+
+    private ParseFile getParseFile(String keyDescription) {
+    }
+
 }
