e0d5622bc87403dedb1a73de4c0ac8b59ad562c2
Linus Brian Okoth <okoth@fb.com>
2020-07-10 15:50:12 -0400

    Added the view

--- app/src/main/java/com/example/linusgram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/linusgram/fragments/ProfileFragment.java
@@ -2,7 +2,9 @@ package com.example.linusgram.fragments;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,6 +51,11 @@ public class ProfileFragment extends HomeFragment {
     }
 
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
