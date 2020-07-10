59bb881be0dbfea8b1355041231ebd3a466e498b
Linus Brian Okoth <okoth@fb.com>
2020-07-10 00:01:21 -0400

    Registered user class

--- app/src/main/java/com/example/linusgram/Models/ParseApplication.java
+++ app/src/main/java/com/example/linusgram/Models/ParseApplication.java
@@ -13,6 +13,7 @@ public class ParseApplication extends Application {
 
         ///Register your parse models
         ParseObject.registerSubclass(Post.class);
+        ParseObject.registerSubclass(User.class);
 
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
