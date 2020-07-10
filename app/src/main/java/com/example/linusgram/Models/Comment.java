596a35c197daf4af197c00d6cac7ad3e15c741f4
Linus Brian Okoth <okoth@fb.com>
2020-07-10 19:43:15 -0400

    Added extension

--- app/src/main/java/com/example/linusgram/Models/Comment.java
+++ app/src/main/java/com/example/linusgram/Models/Comment.java
@@ -2,12 +2,24 @@ package com.example.linusgram.Models;
 
 
 import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
 
 @ParseClassName("Comment")
-public class Comment{
+public class Comment extends ParseObject {
 
     public static final String KEY_POST = "RelatedPost";
-    public static final String KEY_USER = "CommentingUser";
+    public static final String KEY_USER_COMMENT = "CommentingUser";
+    public static final String KEY_BODY = "body";
+
+
+    public String getBody(){
+        return getString(KEY_BODY);
+    }
+    public void setBody(String description){
+        put (KEY_BODY, description);
+    }
+
 
 
 
