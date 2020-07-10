e9386cf792df09c01d72714c9d2cdc36a1236c5a
Linus Brian Okoth <okoth@fb.com>
2020-07-10 11:14:37 -0400

    Imported required classes

--- app/src/main/java/com/example/linusgram/HelperClasses/EndlessRecyclerViewScrollListener.java
+++ app/src/main/java/com/example/linusgram/HelperClasses/EndlessRecyclerViewScrollListener.java
@@ -1,6 +1,9 @@
 package com.example.linusgram.HelperClasses;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 public abstract class EndlessRecyclerViewScrollListener extends RecyclerView.OnScrollListener {
     // The minimum amount of items to have below your current scroll position
