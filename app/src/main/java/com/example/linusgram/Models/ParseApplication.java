b1b751a62e2af76a9faf145d0b4af6b29576a817
Linus Brian Okoth <okoth@fb.com>
2020-07-10 20:50:47 -0400

    Registered the comment class

--- app/src/main/java/com/example/linusgram/Models/ParseApplication.java
+++ app/src/main/java/com/example/linusgram/Models/ParseApplication.java
@@ -13,6 +13,7 @@ public class ParseApplication extends Application {
 
         ///Register your parse models
         ParseObject.registerSubclass(Post.class);
+        ParseObject.registerSubclass(Comment.class);
 
 
         // set applicationId, and server server based on the values in the Heroku settings.
