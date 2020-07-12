e9785ac41f3b39ed88ffd31d52df8a2ddbc8a2ec
Linus Brian Okoth <okoth@fb.com>
2020-07-12 09:18:33 -0500

    Added virtual permission

--- app/src/main/java/com/example/linusgram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/linusgram/fragments/ComposeFragment.java
@@ -195,7 +195,7 @@ public class ComposeFragment extends Fragment {
 
         progressBar.setVisibility(ProgressBar.VISIBLE);
 
-        //TODO ADD DELAY SO THAT PROGRESS BAR CAN BE SEEN
+
         post.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
